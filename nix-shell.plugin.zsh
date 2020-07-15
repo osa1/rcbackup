d3f32e440b2545e412e50f1d9042953591ce7c21
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-07-15 11:24:10 +0300

    zsh: Commit buildShellShim script

--- nix-shell.plugin.zsh
+++ nix-shell.plugin.zsh
@@ -53,7 +53,7 @@ function nix-shell() {
     command nix-shell "$@"
   else
     NIX_SHELL_PACKAGES="$NIX_SHELL_PACKAGES" \
-    NIX_BUILD_SHELL="$NIX_SHELL_PLUGIN_DIR/scripts/buildShellShim.zsh" \
+    NIX_BUILD_SHELL="$NIX_SHELL_PLUGIN_DIR/zsh-nix-shell/buildShellShim.zsh" \
     NIX_EXECUTING_SHELL="$SHELL" \
     command nix-shell "$@"
   fi
