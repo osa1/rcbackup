c2426cc3ee88105bb7d0c3b0920b69c63e116ed5
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-09-23 18:16:12 +0300

    wat: Generate wasm-obdump output as well

--- wat/src/main.rs
+++ wat/src/main.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
-use std::{env, fs, process};
+use std::{env, fs, io, process};
 
 use rayon::prelude::*;
 
@@ -32,25 +32,59 @@ fn main() {
             .arg(out_path)
             .output();
 
-        match output {
-            Ok(ret) => {
-                if !ret.status.success() {
+        let _ = report_cmd_out(in_path, "wasm2wat", output);
+
+        let output = Command::new("wasm-objdump").arg("-d").arg(in_path).output();
+
+        if let Some(output) = report_cmd_out(in_path, "wasm-objdump", output) {
+            let out_path = {
+                let mut out_path = in_path.clone();
+                let _ = out_path.set_extension("wasm-objdump");
+                out_path
+            };
+            match fs::write(&out_path, output.stdout) {
+                Ok(()) => {}
+                Err(err) => {
                     println!(
-                        "Unexpected return value while processing {}: {:?}",
-                        in_path.to_string_lossy(),
-                        ret
+                        "Error while writing wasm-objdump output to file {}: {}",
+                        out_path.to_string_lossy(),
+                        err
                     );
                 }
             }
-            Err(err) => {
-                println!(
-                    "Error while processing {}: {}",
-                    in_path.to_string_lossy(),
-                    err
-                );
-            }
         }
     });
+}
+
+fn report_cmd_out(
+    path: &Path,
+    pgm: &str,
+    ret: io::Result<process::Output>,
+) -> Option<process::Output> {
+    match ret {
+        Ok(ret) => {
+            if ret.status.success() {
+                Some(ret)
+            } else {
+                println!(
+                    "Unexpected return value while running {} on {}: {:?}",
+                    pgm,
+                    path.to_string_lossy(),
+                    ret
+                );
+                None
+            }
+        }
+        Err(err) => {
+            println!(
+                "Error while running {} on {}: {}",
+                pgm,
+                path.to_string_lossy(),
+                err
+            );
+            None
+        }
+    }
 }
 
 fn walk_dir(dir: PathBuf, files: &mut Vec<PathBuf>) {
